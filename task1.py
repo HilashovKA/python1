45ec0314cebc945a4658793b6735cd06443f75d5
lavr174 <lavr174@icloud.com>
2022-10-30 13:59:58 +0300

    phoneBook

--- task1.py
+++
@@ -1,7 +0,0 @@
-day = int(input('Enter day number: '))
-if day > 7 or day < 1:
-     print("Повторите ввод от 1 до 7")
-elif day == 6 or day == 7:
-     print("Выходной")
-else:
-     print("Этот день не выходной")
